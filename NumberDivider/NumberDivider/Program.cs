0773d01357ec2877f19958e82a827bb9db4e4992
AWoLf98 <andresistuk@knu.ua>
2018-01-28 06:27:09 +0200

    Number Divider

---
+++ NumberDivider/NumberDivider/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberDivider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Input number: ");
+            number = Int32.Parse(Console.ReadLine());
+            Algorithm_search_divider(number);
+            Console.ReadLine();
+        }
+        static void Algorithm_search_divider(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if(n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
